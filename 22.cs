9189c7b931939b39be0dddd57dc5d7061cb85432
krissbroo <147873888+krissbroo@users.noreply.github.com>
2023-12-15 18:13:10 +0800

    Add files via upload

---
+++ 22.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 9
+
+int ReadInt(string str)
+{
+   Console.WriteLine(str);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+int FuncAc(int m, int n)
+{
+    if(m == 0) 
+    return (n + 1);
+
+    else if(m > 0 && n == 0 )
+    return FuncAc(m - 1, 1);
+
+    else return FuncAc(m - 1, FuncAc(m, n - 1));
+}
+
+//--------------
+int m = ReadInt("Введите m");
+int n = ReadInt("Введите n");
+int Akkerm = FuncAc(m,n);
+System.Console.WriteLine($"  -> Akkerm(m,n) =  {Akkerm}  ");
---
+++ 33.cs
@@ -0,0 +1,44 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int ReadInt(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+int[] tempArray = new int[size];
+Random rand = new Random();
+
+
+for (int i = 0; i < size; i++)
+{
+tempArray[i] = rand.Next(leftRange, rightRange + 1);
+}
+return tempArray;
+}
+
+void PrintArray(int[] array)
+{
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+int RightArray(int [] array, int i = 0)
+{
+    if(i < array.Length / 2 && i >= 0)
+    System.Console.WriteLine(array[i], i++);
+   return  RightArray(array.Lenght - 1 - i);
+    
+}
+
+
+//---------------
+
+int size = ReadInt("Введите размер массива: ");
+int[] myArray = GenerateArray(size, 1, 10);
+PrintArray(myArray);
+RightArray(array);
